7a65fa9da82f159f5cf94306628038f79247fc02
王康康 <1754393348@qq.com>
2019-03-08 16:51:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 (function (doc, win) {
+  var PlanWidth = 640;//设计图宽度
   var docEl = doc.documentElement,
       resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
       recalc = function () {
           var clientWidth = docEl.clientWidth;
           if (!clientWidth) return;
-          if(clientWidth>=640){
+          if(clientWidth>=PlanWidth){
               docEl.style.fontSize = '100px';
           }else{
-              docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+              docEl.style.fontSize = 100 * (clientWidth / PlanWidth) + 'px';
           }
       };
 
